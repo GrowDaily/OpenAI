836e0078f0f489d15d4931b7af0294f39bca55f6
blackbug-official <blackbugofficial@gmail.com>
2024-03-10 18:27:23 +0530

    added git ignore

--- openai-api-learning.py
+++ openai-api-learning.py
@@ -1,15 +1,21 @@
 # pip install openai
 
 from openai import OpenAI
+import pandas as pd
 
 openAIClient = OpenAI()
 
 response = openAIClient.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "who won 2011 cricket world cup?"}
+    {"role": "system", "content": """you are a data analyst, 
+     for any given topic you will give top  2 characteristics"""},
+    {"role": "user", "content": "dessert"},
+    {"role": "user", "content": "ocean"},
+    {"role": "user", "content": "mountains"}
   ]
 )
 
-ChatCompletion(id='chatcmpl-90ovsRjPUYirPdoVDVZkZvddHpN16', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='India won the 2011 Cricket World Cup. India defeated Sri Lanka in the final to win their second Cricket World Cup title.', role='assistant', function_call=None, tool_calls=None))], created=1709982812, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint='fp_4f0b692a78', usage=CompletionUsage(completion_tokens=26, prompt_tokens=26, total_tokens=52))
+response.choices[0].message.content
+
+pd.DataFrame(response.choices)
